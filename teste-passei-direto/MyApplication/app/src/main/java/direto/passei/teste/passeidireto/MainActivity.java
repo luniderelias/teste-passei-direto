ed05d25a15f3425048a83939ff41dab7c85e5717
Luniderluniderelias@hotmail.com <luniderelias@hotmail.com>
2018-01-25 17:57:21 -0300

    First Commit

---
+++ teste-passei-direto/MyApplication/app/src/main/java/direto/passei/teste/passeidireto/MainActivity.java
@@ -0,0 +1,44 @@
+package direto.passei.teste.passeidireto;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterExtras;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_favorites);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @AfterViews
+    public void afterViews(){
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
